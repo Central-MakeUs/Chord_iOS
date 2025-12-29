1633f1e87a61a571807adbcfddd44cd9ee80b453
Seungwan98 <sinna3210@gmail.com>
2025-12-29 22:18:36 +0900

    Initial Commit

---
+++ CoachCoach/CoachCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachCoachApp.swift
+//  CoachCoach
+//
+//  Created by 양승완 on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CoachCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
