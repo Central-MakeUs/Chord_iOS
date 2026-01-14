57ef72c4d54ccec5bf5ae7306cfc2dd0785b9105
Seungwan98 <sinna3210@gmail.com>
2026-01-14 19:01:34 +0900

    [Chore] Tuist 기반 프로젝트 구성 추가
    - Tuist 워크스페이스 및 프로젝트 설정 파일 추가
    - 멀티 모듈 아키텍처를 위한 기본 구조 구축

---
+++ Tuist/Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+#if TUIST
+import ProjectDescription
+
+let packageSettings = PackageSettings(
+  productTypes: [
+    "ComposableArchitecture": .framework,
+    "Dependencies": .framework,
+    "IdentifiedCollections": .framework,
+    "OrderedCollections": .framework,
+    "Perception": .framework,
+    "PerceptionCore": .framework,
+    "CasePaths": .framework,
+    "CasePathsCore": .framework,
+    "CustomDump": .framework,
+    "XCTestDynamicOverlay": .framework,
+    "IssueReporting": .framework,
+    "IssueReportingPackageSupport": .framework,
+    "Clocks": .framework,
+    "CombineSchedulers": .framework,
+    "ConcurrencyExtras": .framework,
+    "SwiftNavigation": .framework,
+    "SwiftUINavigation": .framework,
+    "UIKitNavigation": .framework,
+    "UIKitNavigationShim": .framework,
+    "Sharing": .framework,
+    "Sharing1": .framework,
+    "Sharing2": .framework,
+    "InternalCollectionsUtilities": .framework
+  ]
+)
+#endif
+
+let package = Package(
+  name: "CoachCoachDependencies",
+  dependencies: [
+    .package(
+      url: "https://github.com/pointfreeco/swift-composable-architecture.git",
+      from: "1.11.0"
+    )
+  ]
+)
---
+++ Workspace.swift
@@ -0,0 +1,18 @@
+import ProjectDescription
+
+let workspace = Workspace(
+  name: "CoachCoach",
+  projects: [
+    "CoachCoach",
+    "DesignSystem",
+    "CoreCommon",
+    "CoreModels",
+    "DataLayer",
+    "FeatureHome",
+    "FeatureMenu",
+    "FeatureIngredients",
+    "FeatureAICoach",
+    "FeatureOnboarding",
+    "FeatureMenuRegistration"
+  ]
+)
